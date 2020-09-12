7fcd9b7587f3622a7735fbc14c49e2f00996fb6e
OsamaZainelabdin <osamazain@outlook.com>
2020-09-12 15:13:54 -0500

    Shortest Unsorted Continuous Subarray
    Given an integer array, you need to find one continuous subarray that if you only sort this subarray in ascending order, then the whole array will be sorted in ascending order, too.
    
    You need to find the shortest such subarray and output its length.
    
    Example 1:
    Input: [2, 6, 4, 8, 10, 9, 15]
    Output: 5
    Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the whole array sorted in ascending order.
    Note:
    Then length of the input array is in range [1, 10,000].
    The input array may contain duplicates, so ascending order here means <=.

---
+++ ShortestUnsortedContinuousSubarray/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ProblemSolving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 3, 3, 2, 4 };
+            Console.WriteLine(FindUnsortedSubarray(nums));
+            Console.ReadLine();            
+        }
+
+        public static int FindUnsortedSubarray(int[] nums)
+        {
+            int ix = -1;
+            int lx = -1;
+            int temp = 0;
+            for (int y = 0;y<nums.Length -1; y++)
+            {
+                for (int x = y+1; x < nums.Length; x++)
+                {
+                    if (nums[y] > nums[x])
+                    {
+                        if (ix == -1)
+                            ix = y;
+                        lx = x;
+                        temp = nums[y];
+                        nums[y] = nums[x];
+                        nums[x] = temp;
+                    }
+                }
+            }
+                        
+            if (ix == -1)
+                return 0;
+
+            return lx - ix + 1;
+        }
+    }
+}
