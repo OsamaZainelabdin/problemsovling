62278306ed3206afbc5131ed84712c03df50ae16
OsamaZainelabdin <osamazain@outlook.com>
2020-09-13 14:19:48 -0500

    Diameter of Binary Tree
    Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
    
    Example:
    Given a binary tree
              1
             / \
            2   3
           / \
          4   5
    Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
    
    Note: The length of path between two nodes is represented by the number of edges between them.

---
+++ DiameterOfBinaryTree/DiameterBinaryTree.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemSolving
+{
+   public class DiameterBinaryTree
+    {
+        public TreeNode treeNode1;
+        int ans;
+        public DiameterBinaryTree()
+        {
+            treeNode1 = new TreeNode();           
+
+            treeNode1.val = 100;
+
+            treeNode1.left = new TreeNode(2);
+            treeNode1.right = new TreeNode(3);
+
+            treeNode1.left.left = new TreeNode(4);
+            treeNode1.left.right = new TreeNode(5);
+
+            treeNode1.right.left = new TreeNode(6);
+            treeNode1.right.right = new TreeNode(7);
+            treeNode1.right.right.right = new TreeNode(8);
+            treeNode1.right.right.right.right = new TreeNode(9);
+        }
+
+
+        public int DiameterOfBinaryTree(TreeNode node)
+        {
+            int depth = 0;
+
+            if (node == null)
+            {
+                return 0;
+            }
+            else
+            {
+                depth = DepthMax(node);
+            }
+
+            return depth;
+
+        }
+
+        public int DepthMax(TreeNode node)
+        {
+            if (node == null)
+                return 0;
+            else
+            {
+                /* compute the depth of each subtree */
+                int lDepth = DepthMax(node.left);
+                int rDepth = DepthMax(node.right);
+
+                // use the larger one 
+                if (lDepth > rDepth)
+                    return (lDepth + 1);
+                else
+                    return (rDepth + 1);
+            }
+        }
+
+    }
+}
